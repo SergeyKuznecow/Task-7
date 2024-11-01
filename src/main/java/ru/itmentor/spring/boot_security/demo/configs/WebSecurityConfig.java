8321c4ff4b99d6bf4ce349217a6dbe32a6db1469
SergeyKuznecow <gfsn@rambler.ru>
2024-11-01 21:06:58 +0300

    01112024

--- src/main/java/ru/itmentor/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -13,22 +13,29 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final SuccessUserHandler successUserHandler;
+    private final SuccessUserHandler successUserHandler;// поле для хранения обьекта успешного входа
 
     public WebSecurityConfig(SuccessUserHandler successUserHandler) {
         this.successUserHandler = successUserHandler;
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {//Переопределяет метод для настройки HTTP-безопасности.
         http
+                //Начинает настройку авторизации запросов.
                 .authorizeRequests()
+      //  Разрешает доступ к корневой странице (/) и странице индекса (/index) всем пользователям.
                 .antMatchers("/", "/index").permitAll()
+       // Требует аутентификации для всех остальных запросов.
                 .anyRequest().authenticated()
+
                 .and()
+    //    Настраивает форму входа и указывает обработчик успешного входа (successUserHandler).
                 .formLogin().successHandler(successUserHandler)
+   //     Разрешает доступ к форме входа всем пользователям.
                 .permitAll()
                 .and()
+//        Настраивает выход из системы и разрешает доступ к нему всем пользователям.
                 .logout()
                 .permitAll();
     }
@@ -36,14 +43,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     // аутентификация inMemory
     @Bean
     @Override
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService() {//Переопределяет метод для настройки сервиса деталей пользователя.
         UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
+                User.withDefaultPasswordEncoder()//Создает пользователя с использованием стандартного кодировщика паролей.
+                        .username("user") //Устанавливает имя пользователя.
                         .password("user")
                         .roles("USER")
-                        .build();
+                        .build();// Создает объект UserDetails.
 
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(user);//Возвращает сервис деталей пользователя, который хранит пользователей в памяти.
     }
 }
